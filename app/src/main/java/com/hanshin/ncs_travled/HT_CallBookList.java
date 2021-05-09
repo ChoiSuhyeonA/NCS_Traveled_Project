81baffddd5fffa170b307483ca2185c70aaa37a5
Choi suhyeon <suhyeon1137@gmail.com>
2021-05-09 12:01:44 +0900

    a

--- app/src/main/java/com/hanshin/ncs_travled/HT_CallBookList.java
+++ app/src/main/java/com/hanshin/ncs_travled/HT_CallBookList.java
@@ -13,7 +13,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
 public class HT_CallBookList extends Activity {
@@ -27,6 +37,11 @@ public class HT_CallBookList extends Activity {
     String loginName ="-";
     String loginEmail = "-";
 
+    //지역, 도시
+    String area;
+    String city;
+
+    ArrayList<String> title;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,38 +68,89 @@ public class HT_CallBookList extends Activity {
 
         Intent getIntent = getIntent();
 
-        String area = getIntent.getStringExtra("nameOfArea");
-        String city = getIntent.getStringExtra("nameOfCity");
+        area = getIntent.getStringExtra("nameOfArea");
+        city = getIntent.getStringExtra("nameOfCity");
+
+        switch (city){
+            case "성남\n용인":
+                city = "성남,용인";
+                break;
+            case "부천 / 광명":
+                city = "부천,광명";
+                break;
+            case "안산 / 안양":
+                city = "안산,안양";
+                break;
+            case "서구\n동구":
+                city = "서구,동구";
+                break;
+            case "계양\n부평":
+                city = "계양,부평";
+                break;
+            case "북구 / 금정":
+                city = "북구,금정";
+                break;
+            case "남포동 / 서면":
+                city = "남포동,서면";
+                break;
+            case "사하구 / 영도 / 남구":
+                city = "사하구,영도,남구";
+                break;
+            case "둔산\n서구":
+                city = "둔산,서구";
+                break;
+            case "대홍\n중구":
+                city = "대홍,중구";
+                break;
+            case "유성구 / 궁동":
+                city = "유성구,궁동";
+                break;
+            case "수성구\n수성못":
+                city = "수성구,수성못";
+                break;
+            case "서구-북구":
+                city = "서구,북구";
+                break;
+            case "중구 / 동성로":
+                city = "중구,동성로";
+                break;
+            case "이월드 / 남구":
+                city = "이월드,남구";
+                break;
+            case "서구\n상무":
+                city = "서구,상무";
+                break;
+            case "북구 / 전남대":
+                city = "북구,전남대";
+                break;
+            case "충장로 / 동명":
+                city = "충장로,동명";
+                break;
+
+        }
         city.replaceAll("", " ");
         areaTv = findViewById(R.id.areaName_tv);
-        areaTv.setText(area);
+        areaTv.setText(area+" - "+city);
 
         adapter = new HT_ListViewAdapter();
         books_lv = findViewById(R.id.books_lv);
         books_lv.setAdapter(adapter);
 
-        //                // 다운로드 테스트
-//                FirebaseStorage storage = FirebaseStorage.getInstance();
-//                StorageReference storageRef = storage.getReference();
-//                String fileName = "suhyeon0";
-//                File fileDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES + "area + "/" + city + "/" + title + "/" + Datename + "-image" + i");
-//                final File downloadFile = new File(fileDir, fileName);
-//
-//               FirebaseStorage storage1 = FirebaseStorage.getInstance();
-//                StorageReference storageReference = storage1.getReference();
-//                StorageReference downloadRef = storageRef.child("060036bd-145e-4fd0-ae22-f577903a1744.png");
-//
-//                downloadRef.getFile(downloadFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-//                @Override
-//                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-//                 Toast.makeText(BT_CreateActivity.this, "다운로드 성공", Toast.LENGTH_SHORT).show();
-//                 Glide.with(BT_CreateActivity.this).load(new File(downloadFile.getAbsolutePath())).into(testimage); }
-//                 }).addOnFailureListener(new OnFailureListener() {
-//                @Override
-//                public void onFailure(@NonNull Exception e) {
-//                    Toast.makeText(BT_CreateActivity.this, "다운로드 실패", Toast.LENGTH_SHORT).show();
-//                }});
 
+//        //파이어베이스 데이터 정보가져오기 오류발생
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        db.collection("loginEmail").document("area").collection("city").document("수현북")
+//                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                DocumentSnapshot document = task.getResult();
+//                BT_Create_Item item = document.toObject(BT_Create_Item.class);
+//
+//                areaTv.setText(item.getTitle());
+//            }
+//        });
+
+        //리스트를 뿌려
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.bookcoverimage1), "Book1", "수원", "AAA", "2020/03/15");
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.bookcoverimage2), "Book2", "서울", "BBB", "2020/02/21");
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.bookcoverimage3), "Book3", "고양", "CCC", "2020/01/04");
