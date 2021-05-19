1cb0a4cdbcf44c9d2b2619668f24dc028ec39e6f
ChoiSuhyeonA <suhyeon1137@gmail.com>
2021-05-19 19:23:27 +0900

    community

--- app/src/main/java/com/hanshin/ncs_travled/CT_Activity.java
+++ app/src/main/java/com/hanshin/ncs_travled/CT_Activity.java
@@ -2,18 +2,33 @@ package com.hanshin.ncs_travled;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.net.URI;
+import java.util.ArrayList;
 
 public class CT_Activity extends Activity {
 
@@ -25,6 +40,20 @@ public class CT_Activity extends Activity {
     //구글로그인 회원정보
     String loginName ="";
     String loginEmail = "";
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    static CT_Create_Item ct_item = new CT_Create_Item();
+
+    //커뮤니티 데이타
+    ArrayList<String> email = new ArrayList<String>();
+    ArrayList<String> name = new ArrayList<String>();
+    ArrayList<String> title = new ArrayList<String>();
+    ArrayList<String> date = new ArrayList<String>();
+    ArrayList<String> contents = new ArrayList<String>();
+    ArrayList<String> pageNumber = new ArrayList<String>();
+    ArrayList<Uri> image = new ArrayList<Uri>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,35 +103,100 @@ public class CT_Activity extends Activity {
         init();
         getData();
 
-        ///////////// 사용자 게시판 
-        listAdapter = new CT_Adapter();
-
-        listview = findViewById(R.id.ct_listview);
-        listview.setAdapter(listAdapter);
-
-        listAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.list1),"Book1","2020/03/15");
-        listAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.list2),"Book2","2020/02/21");
-        listAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.list3),"Book3","2020/01/04");
-        listAdapter.addItem(ContextCompat.getDrawable(this, R.drawable.list4),"Book4","2019/12/23");
-
         //글쓰기 버튼 클릭
         writeBtn = findViewById(R.id.ct_writeBtn);
         writeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(getApplicationContext(),"글 쓰기 버튼",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getApplicationContext(), CT_Create.class);
                 startActivity(intent);
             }
         });
 
+
+
+        //커뮤니티에 등록된 모든 데이타 가져오기기
+        db.collection("community").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    //컬렉션 아래에 있는 모든 정보를 가져온다.
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        ct_item = document.toObject(CT_Create_Item.class);
+                        pageNumber.add(ct_item.getPageNumber());
+                        email.add(ct_item.getEmail());
+                        name.add(ct_item.getName());
+                        title.add(ct_item.getTitle());
+                        date.add(ct_item.getDate());
+                        contents.add(ct_item.getContents());
+                    }
+                    firestoreImageAdd();
+                }
+                else{
+                    Toast.makeText(CT_Activity.this, "로딩실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+            //에러가 발생됐을 경우.
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+                Toast.makeText(CT_Activity.this, "데이터 정보가 없습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 
+    private void firestoreImageAdd() {
+        // 커뮤니티 이미지 다운로드
+        for(int i=0; i<pageNumber.size(); i++){
+            StorageReference storageReference = storage.getReference();
+            StorageReference storageRef = storageReference.child(("community" +"/" + email.get(i) + "/" + pageNumber.get(i) ));
+            storageRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                @Override
+                public void onSuccess(ListResult listResult) {
+                    for (StorageReference item : listResult.getItems()) {
+                        item.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                image.add(uri);
+                                listAdd();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(CT_Activity.this, "데이터 다운로드 실패", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void listAdd() {
+        ///////////// 사용자 게시판
+        listAdapter = new CT_Adapter();
+
+        listview = findViewById(R.id.ct_listview);
+        listview.setAdapter(listAdapter);
+
+        for(int i=0; i<pageNumber.size(); i++){
+            listAdapter.addItem(image.get(i),title.get(i),date.get(i));
+        }
+
+    }
+
+
+
+    //리사이클러뷰
     private void init() {
         recyclerView = findViewById(R.id.ct_recycler);
-
         recyclerView.addItemDecoration(new CT_recyclerDecoration(30)); // 아이템 간격
-
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -120,5 +214,7 @@ public class CT_Activity extends Activity {
         recyclerItem = new CT_recyclerItem(R.drawable.jeju1,"list4","2020/02/12");
         recyclerAdapter.addItem(recyclerItem);
     }
+
+
 
 }
--- app/src/main/java/com/hanshin/ncs_travled/CT_Create.java
+++ app/src/main/java/com/hanshin/ncs_travled/CT_Create.java
@@ -3,25 +3,54 @@ package com.hanshin.ncs_travled;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CT_Create extends Activity {
     Button ct_close_btn, ct_SaveBtn;
     ImageButton ct_createImage;
     EditText ct_createTitle,ct_createDate, ct_createContent;
 
-    //구글로그인 회원정보
+    //선택한 갤러리 이미지
+    Uri image;
+
+    //구글로그인 회원정보 및 데이타
     String loginName ="";
     String loginEmail = "";
+    String pageNumber="";
+
+
+    static CT_Create_Item ct_item = new CT_Create_Item();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,20 +72,21 @@ public class CT_Create extends Activity {
         ct_createTitle = findViewById(R.id.ct_createTitle);
         ct_createDate = findViewById(R.id.ct_createDate);
 
-        String title;
-        String contents;
-        String date;
-        //제목 + 내용 정보를 UI를 통해서 가져온다.
-        title = ct_createTitle.getText().toString();
-        contents = ct_createContent.getText().toString();
-        date = ct_createDate.getText().toString();
-
+        //업로드할때 날짜를 폴더명뒤에 지정해서, 파일을 분류
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd.hh.mm.ss");
+        Date now = new Date();
+        String Datename = formatter.format(now);
+        //커뮤니티 게시글을 분류
+        pageNumber = loginEmail+"|"+Datename;
 
         //갤러리 선택 버튼 클릭시.
         ct_createImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             //갤러리 이동해서 이미지 선택할 수 있게 클릭
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/* ");
+                startActivityForResult(intent, 1000);
             }
         });
 
@@ -72,12 +102,72 @@ public class CT_Create extends Activity {
         ct_SaveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //파이어베이스 데이터 + 이미지 업로드하기
-
-                Intent intent = new Intent(getApplicationContext(), CT_Activity.class);
-                startActivity(intent);
-                finish();
+                //제목 + 내용 정보를 UI를 통해서 가져온다.
+                ct_item.setTitle(String.valueOf(ct_createTitle.getText()));
+                ct_item.setContents(String.valueOf(ct_createContent.getText()));
+                ct_item.setDate(String.valueOf(ct_createDate.getText()));
+                ct_item.setEmail(loginEmail);
+                ct_item.setName(loginName);
+                ct_item.setPageNumber(pageNumber);
+                dataUpload();
             }
         });
+    }
+
+    private void dataUpload() {
+
+
+        Map<String, Object> community = new HashMap<>();
+        community.put("email", ct_item.getEmail());
+        community.put("name", ct_item.getName());
+        community.put("date", ct_item.getDate());
+        community.put("title", ct_item.getTitle());
+        community.put("contents", ct_item.getContents());
+        community.put("pageNumber", ct_item.getPageNumber());
+
+        //파이어베이스 스토어 업로드 (데이터)
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // 파이어스토어 ( 이메일명/ 지역 / 도시 /포토북명으로 데이터 분류)
+        db.collection("community").document().set(community).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(CT_Create.this, "데이터 업로드 성공", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(CT_Create.this, "데이터 업로드 실패", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
+
+        //파이어스토리지 업로드
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        //이미지 리스트를 파이어베이스에 업로드
+
+            StorageReference imageRef = storageRef.child("community" +"/" + loginEmail + "/" + pageNumber ); //파이어베이스에 업로드할 이미지 이름 지정
+            UploadTask uploadTask = imageRef.putFile(image);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(CT_Create.this, "이미지 업로드 성공", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        Intent intent = new Intent(getApplicationContext(), CT_Activity.class);
+        startActivity(intent);
+    }
+
+    //갤러리 생성하기 필요한 메서드
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            image = data.getData();
+            ct_createImage.setImageURI(image);
+        }
     }
 }
